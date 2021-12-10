330a1d611c3524ed9ee0cbf545913e9dc48d243c
Peter_Gamerrr <497155@student.saxion.nl>
2021-12-10 12:46:48 +0100

    made the switch case a lot more readible in ChatPackageHelper

--- ChatApp/src/chatapp/shared/ChatPackageHelper.java
+++ ChatApp/src/chatapp/shared/ChatPackageHelper.java
@@ -11,63 +11,29 @@ public class ChatPackageHelper {
 
         ChatPackage chatPackage = null;
         switch (type) {
-            case BCST:
-                chatPackage = isClient ?
-                        BcstPackage.deserializeClient(packageStr) :
-                        BcstPackage.deserializeServer(packageStr);
-                break;
-            case CONN:
-                chatPackage = ConnPackage.deserialize(packageStr);
-                break;
-            case DSCN:
-                chatPackage = DscnPackage.deserialize(packageStr);
-                break;
-            case ER:
-                chatPackage = ErPackage.deserialize(packageStr);
-                break;
-            case INFO:
-                chatPackage = InfoPackage.deserialize(packageStr);
-                break;
-            case CGRP:
-                chatPackage = CgrpPackage.deserialize(packageStr);
-                break;
-            case JGRP:
-                chatPackage = JgrpPackage.deserialize(packageStr);
-                break;
-            case LGRP:
-                chatPackage = LgrpPackage.deserialize(packageStr);
-                break;
-            case MSG:
-                chatPackage = isClient ?
-                        MsgPackage.deserializeClient(packageStr) :
-                        MsgPackage.deserializeServer(packageStr);
-                break;
-            case OK:
-                chatPackage = OkPackage.deserialize(packageStr);
-                break;
-            case PING:
-                chatPackage = PingPackage.deserialize();
-                break;
-            case PONG:
-                chatPackage = PongPackage.deserialize();
-                break;
-            case QUIT:
-                chatPackage = QuitPackage.deserialize();
-                break;
-            case USR:
-                chatPackage = UsrPackage.deserialize(packageStr);
-                break;
-            case USRS:
-                chatPackage = UsrsPackage.deserialize(packageStr);
-                break;
-            case GRP:
-                chatPackage = GrpPackage.deserialize(packageStr);
-                break;
-            case GRPS:
-                chatPackage = GrpsPackage.deserialize(packageStr);
-                break;
-        }
+            case BCST -> chatPackage = isClient ?
+                            BcstPackage.deserializeClient(packageStr) :
+                            BcstPackage.deserializeServer(packageStr);
 
+            case CONN -> chatPackage = ConnPackage.deserialize(packageStr);
+            case DSCN -> chatPackage = DscnPackage.deserialize(packageStr);
+            case ER -> chatPackage = ErPackage.deserialize(packageStr);
+            case INFO -> chatPackage = InfoPackage.deserialize(packageStr);
+            case CGRP -> chatPackage = CgrpPackage.deserialize(packageStr);
+            case JGRP -> chatPackage = JgrpPackage.deserialize(packageStr);
+            case LGRP -> chatPackage = LgrpPackage.deserialize(packageStr);
+            case MSG -> chatPackage = isClient ?
+                            MsgPackage.deserializeClient(packageStr) :
+                            MsgPackage.deserializeServer(packageStr);
+            case OK -> chatPackage = OkPackage.deserialize(packageStr);
+            case PING -> chatPackage = PingPackage.deserialize();
+            case PONG -> chatPackage = PongPackage.deserialize();
+            case QUIT -> chatPackage = QuitPackage.deserialize();
+            case USR -> chatPackage = UsrPackage.deserialize(packageStr);
+            case USRS -> chatPackage = UsrsPackage.deserialize(packageStr);
+            case GRP -> chatPackage = GrpPackage.deserialize(packageStr);
+            case GRPS -> chatPackage = GrpsPackage.deserialize(packageStr);
+        }
         return chatPackage;
     }
 
