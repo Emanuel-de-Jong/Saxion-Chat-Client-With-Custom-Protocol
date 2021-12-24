2d4f4ba79c95c746a763362d9fdc7766b4de77eb
Peter_Gamerrr <497155@student.saxion.nl>
2021-12-24 12:14:56 +0100

    changed regex to limit group- and username between 3 and 14 char

--- ChatApp/src/chatapp/shared/Globals.java
+++ ChatApp/src/chatapp/shared/Globals.java
@@ -5,7 +5,7 @@ public class Globals {
     public static String publicGroupName = "All";
     public static boolean testing = false;
     public final static int port = 22866;
-    public final static String ALLOWED_CHARACTERS = "^[\\w\\d_]+$";
+    public final static String ALLOWED_CHARACTERS = "^[\\w\\d_]{3,14}$";
 
 
     public Listeners listeners = new Listeners();
